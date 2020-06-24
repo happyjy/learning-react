6ed92d715edecfc9811c357f61a3d47a3124e0ba
happyjy <okwoyjy@gmail.com>
2020-06-25 00:13:00 +0900

    redux-todo del list

---
+++ learning-redux/src/component/ToDo.js
@@ -0,0 +1,24 @@
+import React from "react";
+import { connect } from "react-redux";
+import { actionCreators } from "../store";
+import { Link } from "react-router-dom";
+
+function ToDo({ text, id, onDeleteBtnClick }) {
+  console.log("### ToDo > ToDo: ", { text, id, onDeleteBtnClick });
+  return (
+    <li key={id}>
+      <Link to={`/${id}`}>{text}</Link>
+      <button onClick={onDeleteBtnClick}>DEL</button>
+    </li>
+  );
+}
+
+function mapDispatchToProps(dispatch, ownProps) {
+  console.log("### ToDo > mapDispatchToProps: ", { dispatch, ownProps });
+  return {
+    onDeleteBtnClick: () =>
+      dispatch(actionCreators.deleteToDo(parseInt(ownProps.id))),
+  };
+}
+
+export default connect(null, mapDispatchToProps)(ToDo);
